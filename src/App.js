e888d68869931c4cc528562045a18086d3b732ee
Houssam-nxy <elkhesassi@gmail.com>
2024-03-09 02:47:57 +0100

    1.1.1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Start <code>Quotes</code> project.
+        </p>
+        <a
+          className="App-link"
+          href="##"
+          rel="noopener noreferrer"
+        >
+          Go
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
